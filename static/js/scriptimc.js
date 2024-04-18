86f8bd548109aedc42511f4fe2e386deda554bbf
RadIbn Ahmed <radahmed62@gmail.com>
2024-04-18 09:47:10 +0000

    aggirnamento

--- static/js/scriptimc.js
+++ static/js/scriptimc.js
@@ -1,25 +1,18 @@
-document.getElementById("imcForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    
-    var peso = parseFloat(document.getElementById("peso").value);
-    var altezza = parseFloat(document.getElementById("altezza").value);
-    var età = parseInt(document.getElementById("età").value);
-    
-    var imc = peso / (altezza * altezza);
-    imc = Math.round(imc);
-    
-    var classificazione;
-    if (imc < 18.5) {
-        classificazione = "sottopeso";
-    } else if (imc >= 18.5 && imc < 25) {
-        classificazione = "normopeso";
-    } else if (imc >= 25 && imc < 30) {
-        classificazione = "sovrappeso";
-    } else {
-        classificazione = "obeso";
-    }
-    
-    
-    var messaggioRisultato = "Il tuo IMC è " + imc + ", classificabile come " + classificazione 
-    document.getElementById("risultato").innerText = messaggioRisultato;
-});
+let peso = prompt("Peso","")
+let altezza = prompt("Altezza","")
+
+let imc = peso / (altezza * altezza);
+imc = Math.round(imc);
+
+let classificazione;
+if (imc < 18.5) {
+    classificazione = "sottopeso";
+} else if (imc >= 18.5 && imc < 25) {
+    classificazione = "normopeso";
+} else if (imc >= 25 && imc < 30) {
+    classificazione = "sovrappeso";
+} else {
+    classificazione = "obeso";
+}
+let messaggioRisultato = "Il tuo IMC è " + imc + ", classificabile come " + classificazione 
+alert(messaggioRisultato)
